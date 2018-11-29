a8e87bd748d37a0513eb55485c15acc61c7899dc
edilsonhba3 <38365855+edilsonhba3@users.noreply.github.com>
2018-11-29 16:12:51 -0600

    initial commit
    initial commit

---
+++ ws-restfull-jersy/src/com/tutorialspoint/UserDao.java
@@ -0,0 +1,17 @@
+package com.tutorialspoint;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class UserDao {
+	public List<User> getAllUsers() {
+
+		List<User> userList = null;
+
+		User user = new User(1, "Mahesh", "Teacher");
+		userList = new ArrayList<User>();
+		userList.add(user);
+
+		return userList;
+	}
+}
